99b39939c7c9b29dcf36648c4c45e90b3962366f
Mariusz Biczyk <mariusz-biczyk@hotmail.com>
2016-09-21 19:32:42 +0200

    Added minimal vertex and fragment shader. Used VBO and VAO to draw a single triangle.

--- Applications/Source/main.cpp
+++ Applications/Source/main.cpp
@@ -3,7 +3,22 @@
 #include <GLFW/glfw3.h> // GLFW helper library
 #include <stdio.h>
 
+#include "Shader.h"
+
 GLFWwindow* window = nullptr;
+
+// VAO, VBO
+GLuint VAO;
+GLuint VBO;
+
+Shader* shader;
+
+GLfloat vertices[] = {
+	// Positions         // Colors
+	0.5f, -0.5f, 0.0f,   // Bottom Right
+	-0.5f, -0.5f, 0.0f,  // Bottom Left
+	0.0f,  0.5f, 0.0f,   // Top 
+};
 
 static void init(void)
 {
@@ -24,10 +39,28 @@ static void init(void)
 	// start GLEW extension handler
 	glewExperimental = GL_TRUE;
 	glewInit();
+
+	shader = new Shader("vertex.vert", "fragment.frag");
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 3, (GLvoid*) 0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
 }
 
 void uninitialize(void) 
 {
+	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(1, &VAO);
+
 	glfwTerminate();
 }
 
@@ -37,6 +70,12 @@ void render(void)
 	{
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader->UseProgram();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
