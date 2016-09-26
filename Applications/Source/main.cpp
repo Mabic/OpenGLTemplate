fba869608bdae45965eb462595dca6aee46fee9f
Mariusz Biczyk <mariusz-biczyk@hotmail.com>
2016-09-26 22:48:20 +0200

    Added SOIL library into the project.

--- Applications/Source/main.cpp
+++ Applications/Source/main.cpp
@@ -1,6 +1,7 @@
 #define GLEW_STATIC
 #include <GL/glew.h>    // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
+#include <SOIL.h>
 #include <cmath>
 #include <stdio.h>
 
