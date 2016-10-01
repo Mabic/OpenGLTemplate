a8f14ddd6a3ecb9f162a28562517cb5d046188c2
Mariusz Biczyk <mariusz-biczyk@hotmail.com>
2016-10-01 12:17:55 +0200

    Add new EmptyWindow project

---
+++ Applications/EmptyWindow/Source/main.cpp
@@ -0,0 +1,61 @@
+#define GLEW_STATIC
+#include <GL/glew.h>    // include GLEW and new version of GL on Windows
+#include <GLFW/glfw3.h> // GLFW helper library
+#include <stdio.h>
+
+GLFWwindow* window = nullptr;
+
+static void init(void)
+{
+	// start GL context and O/S window using the GLFW helper library
+	if (!glfwInit()) {
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
+		return;
+	}
+
+	// Set all the required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+	if (!window) {
+		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+		return;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// start GLEW extension handler
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	glViewport(0, 0, 640, 480);
+}
+
+void uninitialize(void) 
+{
+	glfwTerminate();
+}
+
+void render(void)
+{
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
+
+int main() 
+{
+	init();
+	render();
+	uninitialize();
+	return 0;
+}
