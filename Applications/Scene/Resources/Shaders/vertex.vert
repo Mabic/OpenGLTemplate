e997e9ce7c8093cc7115ecaa9d76ab8e1074ffb3
Mariusz Biczyk <mariusz-biczyk@hotmail.com>
2016-12-18 23:07:47 +0100

    Added normals to attributes.

--- Applications/Scene/Resources/Shaders/vertex.vert
+++ Applications/Scene/Resources/Shaders/vertex.vert
@@ -1,7 +1,8 @@
 #version 430 core
 
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 textureCoord;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 textureCoord;
 
 uniform mat4 model;
 uniform mat4 view;
--- Applications/Scene/Source/Object.cpp
+++ Applications/Scene/Source/Object.cpp
@@ -67,6 +67,9 @@ void Object::InitializeBuffers(const Mesh& mesh)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 8, (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 8, (GLvoid*)(sizeof(GL_FLOAT) * 3));
+	glEnableVertexAttribArray(1);
+
 	// Texture
 	if (m_isTextureExist) {
 		glGenTextures(1, &m_textureBufferObject);
@@ -75,8 +78,8 @@ void Object::InitializeBuffers(const Mesh& mesh)
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 8, (GLvoid*)(sizeof(GL_FLOAT) * 6));
-	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 8, (GLvoid*)(sizeof(GL_FLOAT) * 6));
+	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(0);
 
